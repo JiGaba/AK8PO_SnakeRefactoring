2b0b7488c805ff35d243a238df2957889a60208f
JiriGaba <gaba.jiri@seznam.cz>
2024-06-17 23:43:47 +0200

    GameField - přidání vyčištění konzole, vykreslení rámu herní plochy a zprávu o ukončení hry GameEngine - úprava proměnných a konstant

--- Ak8PO_SnakeRefactoring/GameEngine.cs
+++ Ak8PO_SnakeRefactoring/GameEngine.cs
@@ -8,5 +8,126 @@ namespace Ak8PO_SnakeRefactoring
 {
     public class GameEngine
     {
+        private const int _screenHeight = 16;
+        private const int _screenWidth = 32;
+
+        private readonly GameField _gameField;
+        private readonly Random _randomGen;
+
+        private DateTime _timeMainLoop;
+        private DateTime _timeByKeyLoop;
+        private ButtonPressed _buttonPressed;
+        private Direction _movement;
+        private int _score;
+        private bool _gameOver;  
+
+        public GameEngine() 
+        {
+            _gameField = new GameField(_screenHeight, _screenWidth); 
+            _randomGen = new Random();
+            _score = 5;
+            _gameOver = false;
+            _movement = Direction.Right;
+        }
+
+        public void Run()
+        {
+            Pixel hoofd = new Pixel(_screenWidth / 2, _screenHeight / 2, ConsoleColor.Red);
+            
+            List<int> xposlijf = new List<int>();
+            List<int> yposlijf = new List<int>();
+            int berryx = _randomGen.Next(0, _screenWidth);
+            int berryy = _randomGen.Next(0, _screenHeight);
+            
+
+            _gameField.DrawFrame(_screenWidth, _screenHeight);
+
+            while (true)
+            {
+                _gameField.ClearField(_screenWidth, _screenHeight);
+                if (hoofd.XPos == _screenWidth - 1 || hoofd.XPos == 0 || hoofd.YPos == _screenHeight - 1 || hoofd.YPos == 0)
+                {
+                    _gameOver = true;
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                if (berryx == hoofd.XPos && berryy == hoofd.YPos)
+                {
+                    _score++;
+                    berryx = _randomGen.Next(1, _screenWidth - 2);
+                    berryy = _randomGen.Next(1, _screenHeight - 2);
+                }
+                for (int i = 0; i < xposlijf.Count(); i++)
+                {
+                    Console.SetCursorPosition(xposlijf[i], yposlijf[i]);
+                    Console.Write("■");
+
+                    if (xposlijf[i] == hoofd.XPos && yposlijf[i] == hoofd.YPos)
+                    {
+                        _gameOver = true;
+                    }
+                }
+                if (_gameOver) break;
+
+                Console.SetCursorPosition(hoofd.XPos, hoofd.YPos);
+                Console.ForegroundColor = hoofd.ScreenColor;
+                Console.Write("■");
+                Console.SetCursorPosition(berryx, berryy);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("■");
+                _timeMainLoop = DateTime.Now;
+                _buttonPressed = ButtonPressed.No;
+                while (true)
+                {
+                    _timeByKeyLoop = DateTime.Now;
+                    if (_timeByKeyLoop.Subtract(_timeMainLoop).TotalMilliseconds > 500) { break; }
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo toets = Console.ReadKey(true);
+                        if (toets.Key.Equals(ConsoleKey.UpArrow) && _movement != Direction.Down && _buttonPressed == ButtonPressed.No)
+                        {
+                            _movement = Direction.Up;
+                            _buttonPressed = ButtonPressed.Yes;
+                        }
+                        if (toets.Key.Equals(ConsoleKey.DownArrow) && _movement != Direction.Up && _buttonPressed == ButtonPressed.No)
+                        {
+                            _movement = Direction.Down;
+                            _buttonPressed = ButtonPressed.Yes;
+                        }
+                        if (toets.Key.Equals(ConsoleKey.LeftArrow) && _movement != Direction.Right && _buttonPressed == ButtonPressed.No)
+                        {
+                            _movement = Direction.Left;
+                            _buttonPressed = ButtonPressed.Yes;
+                        }
+                        if (toets.Key.Equals(ConsoleKey.RightArrow) && _movement != Direction.Left && _buttonPressed == ButtonPressed.No)
+                        {
+                            _movement = Direction.Right;
+                            _buttonPressed = ButtonPressed.Yes;
+                        }
+                    }
+                }
+                xposlijf.Add(hoofd.XPos);
+                yposlijf.Add(hoofd.YPos);
+                switch (_movement)
+                {
+                    case Direction.Up:
+                        hoofd.YPos--;
+                        break;
+                    case Direction.Down:
+                        hoofd.YPos++;
+                        break;
+                    case Direction.Left:
+                        hoofd.XPos--;
+                        break;
+                    case Direction.Right:
+                        hoofd.XPos++;
+                        break;
+                }
+                if (xposlijf.Count() > _score)
+                {
+                    xposlijf.RemoveAt(0);
+                    yposlijf.RemoveAt(0);
+                }
+            }
+        }
     }
 }
--- Ak8PO_SnakeRefactoring/GameField.cs
+++ Ak8PO_SnakeRefactoring/GameField.cs
@@ -8,5 +8,46 @@ namespace Ak8PO_SnakeRefactoring
 {
     public class GameField
     {
+        public GameField(int screenHeight, int screenWidth) 
+        {
+            Console.WindowHeight = screenHeight;
+            Console.WindowWidth = screenWidth;
+        }
+
+        public void ClearField(int screenwidth, int screenheight)
+        {
+            var blackLine = string.Join("", new byte[screenwidth - 2].Select(b => " ").ToArray());
+            Console.ForegroundColor = ConsoleColor.Black;
+            for (int i = 1; i < screenheight - 1; i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write(blackLine);
+            }
+        }
+
+        public void DrawFrame(int screenwidth, int screenheight)
+        {
+            var horizontalBar = string.Join("", new byte[screenwidth].Select(b => "■").ToArray());
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write(horizontalBar);
+            Console.SetCursorPosition(0, screenheight - 1);
+            Console.Write(horizontalBar);
+
+            for (int i = 0; i < screenheight; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("■");
+                Console.SetCursorPosition(screenwidth - 1, i);
+                Console.Write("■");
+            }
+        }
+
+        public void GameOverMessage(int screenHeight, int screenWidth, int score)
+        {
+            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2);
+            Console.WriteLine("Game over, Score: {0}", score);
+            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2 + 1);
+        }
     }
 }
